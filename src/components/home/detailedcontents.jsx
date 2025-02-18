d20c62739f8933630d4a573718ccae343208a986
promaxpr <info.promaxpr@gmail.com>
2025-02-18 16:14:22 +0530

    8th

--- src/components/home/detailedcontents.jsx
+++ src/components/home/detailedcontents.jsx
@@ -137,7 +137,7 @@ function DetailedContent() {
               <>
                 <div className="position-relative rounded overflow-hidden">
                   <h3 className="mt-2 h3-link">{content.title}</h3> <div style={{ position: "absolute", top: "10px", right: "10px", zIndex: 10 }}>
-                  <ShareDropdown title={content.title} contentId={id} />
+                  <ShareDropdown title={content.title} contentId={id} imageUrl={content.imageUrl} />
     </div>
 
                   <p className="text-muted">{content.subTitle}</p>
