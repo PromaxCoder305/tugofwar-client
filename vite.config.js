1d2895aa480f8d85aa0b9499e215e3288e706b49
promaxpr <info.promaxpr@gmail.com>
2025-02-19 09:57:20 +0530

    19th

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  server: {
+    host: '0.0.0.0', // Ensure Vite binds to all interfaces
+    port: 5173, // Or any other port you're using
+    strictPort: true, // Ensures the port does not change
+    allowedHosts: ['tugofwar-client.onrender.com'], // Allow the Render frontend URL
+  },
+});
